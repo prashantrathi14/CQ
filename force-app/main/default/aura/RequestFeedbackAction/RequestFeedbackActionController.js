156b562b322700ca6c497ef0907ddf2546fc1e04
prashantrathi14 <52100315+prashantrathi14@users.noreply.github.com>
2025-07-03 19:06:25 +0530

    Update RequestFeedbackActionController.js

--- force-app/main/default/aura/RequestFeedbackAction/RequestFeedbackActionController.js
+++ force-app/main/default/aura/RequestFeedbackAction/RequestFeedbackActionController.js
@@ -1,35 +1,36 @@
 ({
-    doInit : function(component, event, helper) {
-    },
-    
     handleRequest : function(component, event, helper) {
         component.set("v.isLoading", true);
-		console.log('v.isLoading>>'+"v.isLoading");
+
         let action = component.get("c.createFeedbackRecord");
         action.setParams({ complaintId: component.get("v.recordId") });
 
         action.setCallback(this, function(response) {
             let state = response.getState();
             let toastEvent = $A.get("e.force:showToast");
-			console.log('state>>>'+state);
+
+            console.log('Apex callback state: ' + state);
+
             if (state === "SUCCESS") {
                 toastEvent.setParams({
                     title: "Success",
                     message: "Feedback request sent successfully.",
                     type: "success"
                 });
-                component.set("v.isLoading", false);
-            	toastEvent.fire();
             } else {
                 let errors = response.getError();
-                console.log('errors>>>>'+errors);
-                let errorMsg = (errors && errors[0] && errors[0].message) ? errors[0].message : "Unknown error occurred.";
+                console.error('Apex error response:', errors);
+                let errorMsg = (errors && errors[0] && errors[0].message)
+                    ? errors[0].message
+                    : "Unknown error occurred.";
+
                 toastEvent.setParams({
                     title: "Error",
                     message: errorMsg,
                     type: "error"
                 });
             }
+            toastEvent.fire();
             
             $A.get("e.force:closeQuickAction").fire();
         });
